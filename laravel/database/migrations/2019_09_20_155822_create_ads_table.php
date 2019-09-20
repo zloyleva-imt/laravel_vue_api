b15496ee6815385efb1ad6967ce424a26e048176
zloyleva <zloyleva.imt@gmail.com>
2019-09-20 19:05:39 +0300

    add Ads

---
+++ laravel/database/migrations/2019_09_20_155822_create_ads_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAdsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ads', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->string('title');
+            $table->text('description');
+            $table->string('img_url');
+            $table->unsignedInteger('user_id');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ads');
+    }
+}
